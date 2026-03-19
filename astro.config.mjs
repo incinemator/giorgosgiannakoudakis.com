1d9e798747760f789218098091e395c48d65d52e
Athena Economides <athena.economides@uzh.ch>
2026-03-19 21:29:54 +0100

    Configure site URL and base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
-  site: 'https://georgiosgiannakoudakis.com',
+  site: 'https://incinemator.github.io',
+  base: '/giorgosgiannakoudakis.com',
   integrations: [
     mdx(),
     tailwind(),
